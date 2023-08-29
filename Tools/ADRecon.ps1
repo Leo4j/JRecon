d8ba9a8ca414b6c80be0357be776fbd9501c61f8
Rob <61951374+Leo4j@users.noreply.github.com>
2023-08-29 10:15:45 +0100

    Add files via upload

--- Tools/ADRecon.ps1
+++ Tools/ADRecon.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 param
 (
     [Parameter(Mandatory = $false, HelpMessage = "Which protocol to use; ADWS (default) or LDAP.")]
@@ -8588,7 +8588,7 @@ Function ConvertFrom-SID
                                     $ADSInitType = $ADS_NAME_INITTYPE_DOMAIN
                                     Try
                                     {
-                                        [System.__ComObject].InvokeMember(“InitEx”,”InvokeMethod”,$null,$Translate,$(@($ADSInitType,$DomainFQDN,($Credential.GetNetworkCredential()).UserName,$DomainFQDN,($Credential.GetNetworkCredential()).Password)))
+                                        [System.__ComObject].InvokeMember("InitEx","InvokeMethod",$null,$Translate,$(@($ADSInitType,$DomainFQDN,($Credential.GetNetworkCredential()).UserName,$DomainFQDN,($Credential.GetNetworkCredential()).Password)))
                                     }
                                     Catch
                                     {
@@ -8602,7 +8602,7 @@ Function ConvertFrom-SID
                                     $ADSInitType = $ADS_NAME_INITTYPE_GC
                                     Try
                                     {
-                                        [System.__ComObject].InvokeMember(“Init”,”InvokeMethod”,$null,$Translate,($ADSInitType,$null))
+                                        [System.__ComObject].InvokeMember("Init","InvokeMethod",$null,$Translate,($ADSInitType,$null))
                                     }
                                     Catch
                                     {
@@ -8613,11 +8613,11 @@ Function ConvertFrom-SID
                                 }
                                 If ($Init)
                                 {
-                                    [System.__ComObject].InvokeMember(“ChaseReferral”,”SetProperty”,$null,$Translate,$ADS_CHASE_REFERRALS_ALWAYS)
+                                    [System.__ComObject].InvokeMember("ChaseReferral","SetProperty",$null,$Translate,$ADS_CHASE_REFERRALS_ALWAYS)
                                     Try
                                     {
-                                        [System.__ComObject].InvokeMember(“Set”,”InvokeMethod”,$null,$Translate,($ADS_NAME_TYPE_UNKNOWN, $TargetSID))
-                                        $ADSOutput = [System.__ComObject].InvokeMember(“Get”,”InvokeMethod”,$null,$Translate,$ADSOutputType)
+                                        [System.__ComObject].InvokeMember("Set","InvokeMethod",$null,$Translate,($ADS_NAME_TYPE_UNKNOWN, $TargetSID))
+                                        $ADSOutput = [System.__ComObject].InvokeMember("Get","InvokeMethod",$null,$Translate,$ADSOutputType)
                                     }
                                     Catch
                                     {
@@ -9380,7 +9380,7 @@ Function Get-ADRDomainAccountsusedforServiceLogon
                 {
                     If ($Credential -ne [Management.Automation.PSCredential]::Empty)
                     {
-                        $session = New-CimSession -ComputerName $hostname -SessionOption $(New-CimSessionOption –Protocol DCOM) -Credential $Credential
+                        $session = New-CimSession -ComputerName $hostname -SessionOption $(New-CimSessionOption -Protocol DCOM) -Credential $Credential
                         If ($session)
                         {
                             $serviceList = @( Get-CimInstance -ClassName Win32_Service -Property Name,StartName,SystemName -CimSession $session -ErrorAction Stop)
@@ -9388,7 +9388,7 @@ Function Get-ADRDomainAccountsusedforServiceLogon
                     }
                     Else
                     {
-                        $session = New-CimSession -ComputerName $hostname -SessionOption $(New-CimSessionOption –Protocol DCOM)
+                        $session = New-CimSession -ComputerName $hostname -SessionOption $(New-CimSessionOption -Protocol DCOM)
                         If ($session)
                         {
                             $serviceList = @( Get-CimInstance -ClassName Win32_Service -Property Name,StartName,SystemName -CimSession $session -ErrorAction Stop )
@@ -9926,29 +9926,6 @@ Function Outvoke-ADRecon
         Catch
         {
             $UseAltCreds = $true
-        }
-    }
-
-    If ($UseAltCreds -and (($DomainController -eq "") -or ($Credential -eq [Management.Automation.PSCredential]::Empty)))
-    {
-
-        If (($DomainController -ne "") -and ($Credential -eq [Management.Automation.PSCredential]::Empty))
-        {
-            Try
-            {
-                $Credential = Get-Credential
-            }
-            Catch
-            {
-                Write-Output "[Outvoke-ADRecon] $($_.Exception.Message)"
-                Return $null
-            }
-        }
-        Else
-        {
-            Write-Output "Run Get-Help .\ADRecon.ps1 -Examples for additional information."
-            Write-Output "[Outvoke-ADRecon] Use the -DomainController and -Credential parameter."`n
-            Return $null
         }
     }
 
